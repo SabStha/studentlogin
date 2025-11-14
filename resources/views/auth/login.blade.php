e835074d6c2a9ca627978b62481e4e99ef9ef3be
Sabin Shrestha <sabstha98@gmail.com>
2025-11-14 15:03:22 +0900

    aa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div style="max-width: 500px; margin: 100px auto;">
     <h1 style="font-size: 32px; color: #333; margin-bottom: 30px; text-align: left;">
-        AIWA FA<span id="quick-login-r" style="cursor: pointer; color: #0066cc; text-decoration: underline; user-select: none;" title="クリックして自動ログイン">R</span>M
+        AIWA FAR<span id="quick-login-m" style="cursor: pointer; color: #0066cc; text-decoration: underline; user-select: none;" title="クリックして自動ログイン">M</span>
     </h1>
     
     <div class="card">
@@ -34,7 +34,7 @@
 </div>
 
 <script>
-document.getElementById('quick-login-r').addEventListener('click', function() {
+document.getElementById('quick-login-m').addEventListener('click', function() {
     // Auto-fill credentials
     document.getElementById('username').value = 'miura';
     document.getElementById('password').value = 'password';
